32894b9bbe28fd85308bafceef0fbaa583851155
JishaVadera <jishamvadera04@gmail.com>
2024-03-16 14:28:08 +0530

    16/03/2024

---
+++ javascript/lec2/main.js
@@ -0,0 +1,91 @@
+// Javascript have two values
+
+// literals :- it has fixed value
+// variables :- it can change the value
+
+// const array = ["hey","hello","how are u"];
+
+// let array = ["hey","hello","how are u"];
+// console.log(array);
+
+// Javascript Sycronomous
+
+// windows.alert()
+// alert()
+
+// alert in javascript
+
+// alert("warning");
+
+// console.log("option-1");
+// console.log("option-2");
+// console.log("option-3");
+// function greey(){
+//     console.log("hello");
+// }
+// greey();
+
+// Javascript Asycronomous
+
+// console.log("option-1");
+// console.log("option-2");
+// console.log("option-3");
+// setTimeout(() => {
+//     console.log("hi,GM");
+// },5000)
+// function greey(){
+//          console.log("hello");
+//     }
+//      greey();
+
+// Operators
+
+// Arithmetic operators
+
+let x=1
+let y=2
+
+// addition
+
+console.log(x+y);
+console.log(y+x);
+
+//Substraction
+
+console.log(x-y);
+console.log(y-x);
+console.log(x-(-y));
+
+// multiplication
+
+console.log(x*y);
+console.log(y*x);
+
+// division
+
+console.log(x/y);
+console.log(y/x);
+
+// modules
+
+console.log(x%y);
+console.log(y%x);
+
+// expoitation
+
+console.log(x**y);
+console.log(y**x);
+
+// increment
+
+console.log(++x);
+console.log(++y);
+console.log(x++);
+console.log(y++);
+
+// decrement
+
+console.log(--x);
+console.log(--y);
+console.log(x--);
+console.log(y--);
