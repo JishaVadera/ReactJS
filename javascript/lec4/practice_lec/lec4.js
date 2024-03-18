6369dd66100b72a5423d38c7059166187abba494
JishaVadera <jishamvadera04@gmail.com>
2024-03-19 00:56:14 +0530

    19/3/2024

---
+++ javascript/lec4/practice_lec/lec4.js
@@ -0,0 +1,95 @@
+// javascript data types
+/*
+1. String
+2. Number
+3. Bigint
+4. Boolean
+5. Undefined
+6. Null
+7. Symbol
+8. Object
+9. Array
+10. function
+*/ 
+
+// primitive data types
+
+/*
+1. String
+2. Number
+3. Bigint
+4. Boolean
+5. Undefined
+6. Null
+7. Symbol
+*/
+
+// non primitive data type
+// 1. Array
+// 2. function
+// 3. Object
+
+// logical operator
+
+// logical AND operator
+
+// let a = true;
+// let b = false;
+// let c = true;
+
+// console.log(a&&b);
+// console.log(a&&b&&c);
+// console.log(a&&c);
+// console.log(c&&b);
+
+
+// logical or operator
+
+// let a = true;
+// let b = false;
+// let c = false;
+
+// console.log(a||b);
+// console.log(a||b||c);
+// console.log(a||c);
+// console.log(c||b);
+
+// logical not operator
+
+// let a = false;
+// let b = false;
+// let c = true;
+
+// console.log(!(a));
+// console.log(!(b));
+// console.log(!(c));
+// console.log(!(a&&b&&c));
+// console.log(!(a&&c));
+// console.log(!(c&&b));
+// console.log(!(a||b));
+// console.log(!(a||b||c));
+// console.log(!(a||c));
+// console.log(!(c||b));
+// console.log(!(c&&b||a));
+// console.log(!(a&&b||c));
+// console.log(!(c&&a||b));
+
+// ternary operator
+
+// let a = 10;
+//     let b = 20;
+    
+//     let c = ( a > b ) ? a : b ;
+//     console.log(c);
+    
+//     let d = ( a < b ) ? a : b ;
+//     console.log(d);
+
+// bitwise operator
+
+// let a = 0
+// let b = 1
+
+// console.log(a<b);
+// console.log(!(a&b));
+
---
+++ javascript/lec4/work/index.js
@@ -0,0 +1,110 @@
+// Create a program that performs arithmetic operations on two numbers.
+
+// {
+//     let a=10;
+//     let b=20;
+    
+//     let add = a+b;
+//     console.log(add);
+    
+//     let sub = a-b;
+//     console.log(sub);
+    
+//     let multi = a*b;
+//     console.log(multi);
+    
+//     let divi = a/b;
+//     console.log(divi);
+    
+//     let mudo = a%b;
+//     console.log(mudo);
+    
+//     let expo = a**b;
+//     console.log(expo);
+// }
+
+// Use compound assignment operators to update variables.
+
+{
+    // var a = 60;
+    // var b = 20;
+    
+    // var add = a+=b;
+    // console.log(add);
+    
+    // var sub = a-=b;
+    // console.log(sub);
+    
+    // var multi = a*=b;
+    // console.log(multi);
+    
+    // var divi = a/=b;
+    // console.log(divi);
+    
+    // var mudo = a%=b;
+    // console.log(mudo);
+    
+    // var expo = a**=b;
+    // console.log(expo);
+}
+
+// Compare two numbers and display the result.
+
+{
+    // let j=4
+    // let s=2
+    
+    // let o = j==s;
+    // console.log(o);
+    
+    // let z= j!=s;
+    // console.log(z);
+    
+    // let c= j===s;
+    // console.log(c);
+    
+    // let d= j!==s;
+    // console.log(d);
+    
+    // let e= j<s;
+    // console.log(e);
+    
+    // let f= j<=s;
+    // console.log(f);
+    
+    // let g= j>s;
+    // console.log(g);
+    
+    // let h= j>=s;
+    // console.log(h);
+}
+
+// Use the ternary operator to check a condition.
+
+// (condition)?true:false
+
+// {
+//     let a = 10;
+//     let b = 20;
+    
+//     let c = ( a > b ) ? a : b ;
+//     console.log(c);
+    
+//     let d = ( a < b ) ? a : b ;
+//     console.log(d);
+// }
+
+// Compare two strings and display the result.
+
+{
+    let st1 = "hello";
+    let st2 = "skillqode";
+    let st5 = "hello";
+    
+    let st3 = st1===st2;
+    console.log(st3);
+    
+    let st4 = st1===st5;
+    console.log(st4);
+
+}
