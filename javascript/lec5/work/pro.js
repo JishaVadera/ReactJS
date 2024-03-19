e2544cc7799ea180e5532355336a6e533d116722
JishaVadera <jishamvadera04@gmail.com>
2024-03-20 00:47:32 +0530

    20/03/2024

---
+++ javascript/lec5/work/pro.js
@@ -0,0 +1,99 @@
+// program that checks if the number is positive, negative or zero
+// number declared here
+// let number = 200;
+
+// check if number is greater than 0
+// if (number > 0) {
+//     console.log("The number is positive");
+// }
+
+// check if number is 0
+// else if (number == 0) {
+//   console.log("The number is zero");
+// }
+
+// if number is less than 0
+// else {
+//      console.log("The number is negative");
+// }
+
+
+// Check if a person is eligible to vote
+
+// let age = 18;
+
+// if(age>=18)
+// {
+//     console.log("Eligible for Voting!");
+// }
+// else{
+//     console.log("Not Eligible for Voting!");
+// }
+
+
+
+
+
+// -------------Determine the largest of three numbers-----------
+// let a = 10;
+// let b = 20;
+// let c = 30;
+
+// if(a >= b && a >= c){
+//     console.log("Largest Number of A")
+// }
+// else if(b >= a && b>=c){
+//     console.log("Largest Number of B")
+// }
+// else{
+//     console.log("Largest Number of C")
+// }
+
+
+
+
+
+// ----------Check if a year is a leap year--------
+
+// let year = 2000;
+
+// if (year % 400 == 0) {
+//     console.log("this year is a leap year");
+//  }
+//  else if (year % 100 == 0){
+//     console.log("this year is a not leap year");
+//  }
+//  else if (year % 4 == 0){
+//     console.log("this year is a leap year");
+//  }
+//  else{
+//     console.log("this year is a not leap year");
+//  }
+
+
+
+// ---------Determine the grade based on a score----------
+
+// let marks = 60;
+
+// if(marks>=40 && marks<50){
+//     console.log("grade f");
+// }
+// else if(marks>=50 && marks<60){
+//    console.log("grade e") ;
+// }
+// else if(marks>=60 && marks<70){
+//     console.log("grade d") ;
+//  }
+// else if(marks>=70 && marks<80){
+//     console.log("grade c") ;
+//  }
+//  else if(marks>=80 && marks<90){
+//     console.log("grade b") ;
+//  }
+//  else if(marks>=90 && marks<100){
+//     console.log("grade a") ;
+//  }
+//  else{
+//     console.log("fail") ;
+//  }
