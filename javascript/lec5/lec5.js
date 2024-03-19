18b1527ce8d8508bf66795da859ec8a51f5d26a5
JishaVadera <jishamvadera04@gmail.com>
2024-03-19 15:47:27 +0530

    19/03/2024

---
+++ javascript/lec5/lec5.js
@@ -0,0 +1,121 @@
+// Javascript string
+// let string1 = "Hello, Good Morning!";
+// let string2 = "How are you!!";
+// let string3 =  string1 + string2;
+// console.log(string3);
+
+// typeof operator
+
+    // let str = "Hello"
+    // let num = 12345
+    // let boolean = true
+    // let array = []
+    // let obj = {}
+    // let big = 123456889n
+    // let fun = function(){};
+    // let n = null;
+    // let un = undefined;
+    // let sym = Symbol("datatype");
+    
+    // console.log(typeof(str));
+    // console.log(typeof(num));
+    // console.log(typeof(boolean));
+    // console.log(typeof(array));
+    // console.log(typeof(obj));
+    // console.log(typeof(big));
+    // console.log(typeof(fun));
+    // console.log(typeof(n));
+    // console.log(typeof(un));
+    // console.log(typeof(sym));
+
+// Javascript statements
+
+/*
+
+if statement
+
+if else statement
+
+if else if statment
+
+nested statement
+
+switch statement
+
+*/
+
+// if statement
+
+// let std = 9;
+
+// if (std < 10) {
+//     console.log("you are not eligiable for higher education");
+// }
+
+// if else statement
+
+// let std = 9;
+
+// if (std < 10) {
+//     console.log("you are not eligible for higher education!");
+// }
+// else {
+//     console.log("you are eligible for higher education!!");
+// }
+
+
+// if else if statement
+
+// let std = 12;
+
+// if(std >= 12){
+//     console.log("you are eligible for degree education");
+// }
+// else if(std >= 10){
+//     console.log("you are eligible for diploma education");
+// }
+// else{
+//     console.log("you are not eligible for degree and diploma education");
+// }
+
+// nested statements
+
+// let age = 12;
+
+// if(age > 18){
+//     if (age > 18) {
+//         console.log("you are eligible for voting");   
+//     }
+//      else {
+//         console.log("you are not eligible for voting");
+//     }
+// }
+// else{
+//     console.log("your age is not sufficient for voting");
+// }
+
+// switch statement
+
+// let book = "biology";
+
+// switch (book) {
+//     case "maths":
+//         console.log("this book is available");
+//         break;
+
+//     case "Hindi":
+//         console.log("this book is available");
+//         break;
+
+//     case "science":
+//         console.log("this book is available");
+//         break;
+
+//     case "gujarati":
+//         console.log("this book is available");
+//         break;
+
+//     default:
+//         console.log("Any book is not available");
+//         break;
+// }
