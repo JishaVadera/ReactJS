8cf4cbbb701d27403d69d7562c8d3ac07c283665
JishaVadera <jishamvadera04@gmail.com>
2024-03-22 15:29:36 +0530

    22/03/2024

---
+++ javascript/lec6/lec6.js
@@ -0,0 +1,105 @@
+// Javascript Template  Literals
+
+// let string1 = "Hey, How are you??";
+
+// let string2 = "Hey, 'How' are you??";
+
+// let string3 =  `Hey, How are you??`;
+
+// let string4 = 'Hey, "How" are you??';
+
+// let string5 = "Hey, \"How\" are you??";
+
+// let string6 = 'Hey, \'How\' are you??';
+
+// let string7 = `Hey, \`How\` are you??`;
+
+// console.log(string1);
+// console.log(string2);
+// console.log(string3);
+// console.log(string4);
+// console.log(string5);
+// console.log(string6);
+// console.log(string7);
+
+// let string1 = `hey, 'how' 
+// are you?`
+
+// console.log(string1);
+
+// passing the variable
+
+// let surname = "Vadera";
+// let name = "Jisha ${surname}";
+// console.log(name);
+
+// let name1 = `Jisha ${surname}`;
+// console.log(name1);
+
+// javascript loop
+/*
+while loop
+do while loop
+for loop
+for in.... loop
+for of...... loop
+*/
+
+// while loop
+// while(condition){
+//     statement;
+// }
+
+// let num= 0 ;
+
+// while(num < 10)
+// {
+    // console.log("Hello world!");
+    // document.write("hello world!!"+ "<br>");
+    // document.write(`hello world!! ${num}`+ "<br>");
+    // document.getElementById("demo").innerHTML="hello world!!"+ num;
+    // num++;
+// }
+
+// do-while statement
+
+// do {
+//     statement;
+// } while (condition);
+
+// let i = -10;
+// do {
+//     document.write(`loop! ${i}` + "<br>");
+//     i++;
+// } while (i <= 10);
+
+// for loop
+
+/*
+for (initialization; condition; although){
+    statements
+}
+*/
+
+let i = 0;
+let n = 10;
+// for (i ;i < n ; i++) {
+//     console.log("jsk");
+    
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log("jsk");  
+// }
+
+// for ( i = 10; i < 10; i++) {
+//     console.log("jsk");  
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log("jsk");  
+// }
+
+// for (const i = 0; i < 10; i++) {
+//     console.log("jsk");  
+// }
