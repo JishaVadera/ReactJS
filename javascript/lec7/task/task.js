1ec12c3749fdb94575cf12194f51fff6c4aa14bc
JishaVadera <jishamvadera04@gmail.com>
2024-03-25 23:10:54 +0530

    25/03/2024

--- javascript/lec7/task/task.js
+++ javascript/lec7/task/task.js
@@ -21,7 +21,7 @@
 
 // 4 — Print the EVEN numbers from 10 to -20.
 
-//   for(let i = 10; i <= 20; i++){ 
+//   for(let i = -20; i <= 10; i++){ 
 //     if(i % 2 == 0){
 //      console.log(i)
 //     }
