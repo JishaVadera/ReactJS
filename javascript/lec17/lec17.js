83162d4c100346ec51638fb2d72b5904337c44d0
JishaVadera <jishamvadera04@gmail.com>
2024-04-09 20:41:38 +0530

    09/04/2024

---
+++ javascript/lec17/lec17.js
@@ -0,0 +1,37 @@
+// Javascript Math object method
+
+let Maths1 = Math.round(12.36);
+console.log(Maths1);
+
+let Maths2 = Math.sign(-56);
+console.log(Maths2);
+
+let Maths3 = Math.sign(89);
+console.log(Maths3);
+
+let Maths4 = Math.sign(0);
+console.log(Maths4);
+
+let Maths5 = Math.max(12,65,24,87,100,4554);
+console.log(Maths5);
+
+let Maths6 = Math.min(12,65,78,32,74,100);
+console.log(Maths6);
+
+let Maths7 = Math.sin( 90 * 3.14 / 180 );
+console.log(Maths7);
+
+let Maths8 = Math.cos( 90 * 3.14 / 180);
+console.log(Maths8);
+
+let Maths9 = Math.random();
+console.log(Maths9);
+
+let Maths10 = Math.floor(Math.random() * 100 ) + 1;
+console.log(Maths10);
+
+let max = 100;
+let min = 91;
+
+let Maths11 = Math.floor(Math.random() * (max - min + 1 )) + min;
+console.log(Maths11);
