a0557a650f270cbf57eecd01e9b8d39aad66ba59
JishaVadera <jishamvadera04@gmail.com>
2024-04-22 15:04:34 +0530

    22/04/2024

---
+++ javascript/lec25/lec25.js
@@ -0,0 +1,58 @@
+// Javascript HTML DOM Method
+// DOM selection Method
+
+let dom1 = document.getElementById('demo')
+console.log(dom1);
+
+let dom2 = document.getElementById('demo').id
+console.log(dom2);
+
+let dom3 = document.getElementById('demo').className;
+console.log(dom3);
+
+// style
+
+// document.getElementById("demo").style.color = "blue";
+
+// let dom4 = document.getElementsByClassName("heading")
+
+// dom4[0].style.color = "green"
+// console.log(dom4);
+
+// let dom5 = document.getElementById("demo").style.backgroundColor = "blue";
+// console.log(dom5);
+
+// textcontent , innerText and innerHTML
+
+// let dom6 = document.getElementById('demo').textContent;
+// console.log(dom6);
+
+// let dom7 = document.getElementById('demo').innerText;
+// console.log(dom7);
+
+// let dom8 = document.getElementById('demo').innerHTML;
+// console.log(dom8);
+
+// QuerySelector and QuerySelectorAll
+
+// let data1 = document.querySelector('h1');
+// console.log(data1);
+
+// data1.style.color = "purple";
+
+// let data2 = document.querySelector('li');
+// console.log(data2);
+
+// data2.style.color = "purple";
+
+// let data3 = document.querySelectorAll("li");
+// console.log(data3);
+
+// data3.forEach((li) => {
+//     li.style.color = "red";
+// })
+
+// let data4 = document.querySelector('input[type = "password"]');
+// console.log(data4);
+
+// data4.style.backgroundColor = "purple";
